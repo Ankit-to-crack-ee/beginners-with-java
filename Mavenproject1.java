fbb7214a1ef84559da2df669b30606c114293bcd
Ankit-to-crack-ee <109519680+Ankit-to-crack-ee@users.noreply.github.com>
2022-07-18 16:24:51 +0530

    Add files via upload

---
+++ Mavenproject1.java
@@ -0,0 +1,21 @@
+package com.mycompany.mavenproject1;
+import java.util.Scanner;
+public class Mavenproject1 {
+
+    public static void main(String[]args) {
+        
+            Scanner sc=new Scanner(System.in);
+        int n=sc.nextByte();
+        
+        for(int i=0;i<n;i++)
+                {
+                    for(int k=i;k>0;k--)
+                            {
+                                System.out.print(i);
+                                  
+                            }
+                    System.out.println();
+                }
+                
+    }
+}
