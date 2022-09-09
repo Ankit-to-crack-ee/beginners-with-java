dc9def68010a247213384cf23fbe723b0b4d2fa3
Ankit-to-crack-ee <109519680+Ankit-to-crack-ee@users.noreply.github.com>
2022-09-09 22:20:44 +0530

    Add files via upload

---
+++ Gta.java
@@ -0,0 +1,30 @@
+
+
+package com.mycompany.gta;
+class Tommy{
+    public  void hitting()
+    {
+        System.out.println("tommy hitting");
+    }
+     public void fighting()
+     {
+         System.out.println("tommy is fighting");
+     }
+     public void running()
+     {
+         System.out.println("running");
+     }
+     
+}
+
+public class Gta {
+
+    public static void main(String[] args) {
+        Tommy gta = new Tommy();
+        gta.hitting();
+        gta.fighting();
+        gta.running();
+        
+        
+    }
+}
